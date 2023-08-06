04bfbe40a1fda396fb3dbc8c4aa8a6eef7e41aa3
Seth Kwame Addo <addoseth7@gmail.com>
2023-08-06 12:59:23 +0100

    fixed the server

--- server/models/Books.js
+++ server/models/Books.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const newBook = new Schema({
-    title: String,
-    author: String,
-    year: Number
+  title: String,
+  author: String,
+  year: Number,
 });
 
-const Book = mongoose.model("AllBooks",newBook);
+const Book = mongoose.model("Books", newBook);
 
 module.exports = Book;
--- server/seed.js
+++ server/seed.js
@@ -4,11 +4,23 @@ mongoose.connect(process.env.DATABASE_URL);
 const book = require("./models/Books");
 
 async function seed() {
-  await book.create({ title: "Good Man", author: "John Wick", year: "1992" });
+  await book.create({
+    title: "Good Man",
+    author: "John Wick",
+    year: "1992",
+  });
 
-  await book.create({ title: "Good Man", author: "John Wick", year: "1992" });
+  await book.create({
+    title: "Good Man",
+    author: "John Wick",
+    year: "1992",
+  });
 
-  await book.create({ title: "Good Man", author: "John Wick", year: "1992" });
+  await book.create({
+    title: "Good Man",
+    author: "John Wick",
+    year: "1992",
+  });
   console.log("database up and running");
   mongoose.disconnect();
 }
